1776696fcd2eb1c8ed94f5d0df7b6adbbc0183aa
Karan Singh Negi <46072159+The-lost-cause@users.noreply.github.com>
2020-08-24 23:04:28 +0530

    LifeCycle Methods: During UPDATE

---
+++ 23. LifeCycle Update Hooks/src/components/Persons/Persons.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import Person from './Person/Person'
+
+class Persons extends Component {
+    state = {};
+    static getDerivedStateFromProps(props, state) {
+        console.log('[Persons.js] getDerivedStateFromProps called....');
+        return state;
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate called..');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getsSnapshotBeforeUpdate called...');
+        return { message: 'Snapshot'};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate called....');
+        console.log(snapshot);
+    }
+
+    render() {
+        console.log('[Persons.js] Child Component Rendering..')
+        return this.props.persons.map((person, index) => {
+            return <Person
+                key={person.id}
+                click={() => this.props.clicked(index)}
+                name={person.name} 
+                age={person.age}
+                changed={(event) => this.props.changed(event, person.id)} /> 
+            });
+     } 
+   
+}
+
+
+  export default Persons;
