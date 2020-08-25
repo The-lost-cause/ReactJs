8d764ee1c966ad1397561a55f64179e8c6178ab6
Karan Singh Negi <46072159+The-lost-cause@users.noreply.github.com>
2020-08-25 23:18:43 +0530

    Context API Understanding

---
+++ 30. ContextAPI Understanding/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useEffect, useRef } from 'react';
+import classes from './Cockpit.module.css';
+import AuthContext from '../context/auth-context';
+
+// useEffect Hook will by default take a function and will be called for every render cycle.
+
+const Cockpit = (props) => {
+
+  const toggleBtnRef = useRef(null);
+
+  // Can send HTTP request inside this hook
+  useEffect(()=>  { 
+    console.log('[Cockpit.js] useEffect React Hook running...') 
+    setTimeout(() => {
+      alert('Saved Data To Cloud')
+    }, 2000)
+  }, [props.persons]);
+
+  // Showing How can we even stop after first render Cycle
+  useEffect(()=> {
+    alert('Yo Rendered only Once');
+    toggleBtnRef.current.click();
+  }, []);
+
+  // Cleanup in UseEffect
+  useEffect(()=>  { 
+    console.log('[Cockpit.js] useEffect React Hook running...') 
+    setTimeout(() => {
+      alert('Saved Data To Cloud 2 ')
+    }, 2000)
+ 
+    return () => { console.log('[Cockpit.js] CleanUp Rendering..')}
+  }, []);
+
+    const assignedClasses = [];
+    let btnClass = '';
+
+    if (props.showPersons) {
+        btnClass = classes.Red;
+    }
+
+    if (props.personsLength <= 2) {
+      assignedClasses.push(classes.red);
+    }
+
+    if (props.personsLength <=1) {
+      assignedClasses.push(classes.bold)
+    }
+
+    return (
+        <div className={classes.Cockpit}>
+            <h1 className={assignedClasses.join(' ')}>{props.author  }</h1>
+            <button
+              ref={toggleBtnRef} 
+              className={btnClass}
+              onClick={props.clicked}>Toggle
+            </button>
+            <AuthContext.Consumer>
+              {(context) =><button onClick={context.login}>Log In</button>}
+            </AuthContext.Consumer>
+        </div>
+    );
+}
+
+
+
+export default React.memo(Cockpit) ;
---
+++ 30. ContextAPI Understanding/src/components/Persons/Person/Person.js
@@ -0,0 +1,51 @@
+
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Aux'; 
+import withClass from '../../../hoc/withClass';
+import classes from './Person.module.css';
+import PropTypes from 'prop-types';
+import AuthContext from '../../context/auth-context';
+
+
+class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+    componentDidMount() {
+        //this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
+    render() {
+        console.log('[Person.js] Child Component Rendering..');
+        return (
+            <Aux>
+                <AuthContext.Consumer>
+                    {(context) => 
+                        context.authenticated ? <p>Authenticated</p> : <p>Please Log In</p> }
+                </AuthContext.Consumer>
+                
+                <p key='2' onClick={this.props.click}>I'm {this.props.name} and {this.props.age} years old</p>
+                <input
+                 // ref={(inputEl)=> {this.inputElement = inputEl}}
+                  ref={this.inputElementRef}
+                  key='1' 
+                  type="text" 
+                  onChange={this.props.changed} 
+                  value={this.props.name}>
+                </input>
+            </Aux>     
+        )
+    }
+};
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
+
+export default withClass(Person, classes.Person);
+
